1ea617593c6c15d19bf6e2e2da8d6f72ac9a6b62
Jamshed Panthaki <jpanthak@andrew.cmu.edu>
2025-03-26 17:23:02 -0400

    kalman filter

---
+++ kalman.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class KalmanFilter:
+    #implemented thanks to https://thekalmanfilter.com/kalman-filter-explained-simply/
+    def __init__(
+        self, 
+        x0=np.array([[-70.]]), 
+        P0=np.array([[5.]]), 
+        A=np.array([[1.]]), 
+        H=np.array([[1.]]), 
+        Q=np.array([[0.1]]), 
+        R=np.array([[2.]])
+    ):
+        #unsure about default values right now
+        
+        self.x = x0
+        self.P = P0
+        self.A = A
+        self.H = H
+        self.Q = Q
+        self.R = R
+
+    def predict(self):
+
+        #update system state
+        self.x = self.A @ self.x
+
+        #update system state error covariance
+        self.P = self.A @ self.P @ self.A.T + self.Q
+
+    def update(self, z):
+        
+        #kalman gain
+        K = (self.P @ self.H.T) @ np.linalg.inv(self.H @ self.P @ self.H.T + self.R)
+
+        #system state estimation
+        self.x = self.x + K @ (z - self.H @ self.x)
+
+        #system state error covariance estimation
+        self.P = self.P - K @ self.H @ self.P
+
+    def step(self, z):
+
+        self.predict()
+        self.update(z)
+
+        return self.x, self.P
